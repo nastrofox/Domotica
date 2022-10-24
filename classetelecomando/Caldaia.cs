f4fa5c4debf43bdc156e4b1b3de5797864b0faf6
Crotti.19315 <Crotti.19315@LABI1-6PC28>
2022-10-24 08:05:46 +0200

    .0

---
+++ classetelecomando/Caldaia.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classetelecomando
+{
+    public class Caldaia
+    {
+        //attributi
+        private bool stato;
+        private double temperatura;
+        private string modello;
+        private string produttore;
+        private string data;
+        private int ndiserie;
+
+        //costruttori
+        public Caldaia(string Modello, string Produttore, string Data, int NumDiSerie)
+        {
+            modello = Modello;
+            produttore = Produttore;
+            data = Data;
+            ndiserie = NumDiSerie;
+        }
+
+        //metodi
+        public void setStato(bool Stato)
+        {
+            stato = Stato;
+        }
+        public void setTemperatura(double Temperatura)
+        {
+            temperatura = Temperatura;
+        }
+        public bool getStato()
+        {
+            return stato;
+        }
+        public double getTemperatura()
+        {
+            return temperatura;
+        }
+        public string getModello()
+        {
+            return modello;
+        }
+        public string getProduttore()
+        {
+            return produttore;
+        }
+        public string getData()
+        {
+            return data;
+        }
+        public int getNdiserie()
+        {
+            return ndiserie;
+        }
+    }
+}
---
+++ classetelecomando/termostato.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classetelecomando
+{
+    public class Termostato
+    {
+        //attributi
+        private string modello;
+        private string produttore;
+        private int ndiserie;
+        private Caldaia caldaia;
+
+        //costruttori
+        public Termostato(string Modello, string Produttore, int NumDiSerie, Caldaia caldaia1)
+        {
+            modello = Modello;
+            produttore = Produttore;
+            ndiserie = NumDiSerie;
+            caldaia = caldaia1;
+        }
+
+        //metodi
+        public void accendi()
+        {
+            caldaia.setStato(true);
+        }
+        public void spegni()
+        {
+            caldaia.setStato(false);
+        }
+        public void setTemperatura()
+        {
+            //prendere il valore ...
+            caldaia.setTemperatura(3);
+        }
+        public string getModello()
+        {
+            return modello;
+        }
+        public string getProduttore()
+        {
+            return produttore;
+        }
+        public int getNdiserie()
+        {
+            return ndiserie;
+        }
+    }
+}
